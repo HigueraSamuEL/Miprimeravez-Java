8195f90f223eaf2471ec322dad674993d3e79b0a
HigueraSamuEL <samuel.higuera@icloud.com>
2026-08-18 19:47:14 -0500

    Subiendo app MiClse

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a = 10, b = 3;
+        System.out.println( a + b );
+        System.out.println( a - b);
+        System.out.println( a * b);
+        System.out.println(a / b );
+        System.out.println( a % b );
+        System.out.println(10.0 / 3);
+
+
+    }
+
+}
---
+++ src/MiClase.java
@@ -0,0 +1,9 @@
+public class MiClase {
+    private int edad ;
+    private double altura ;
+    private char sexo = 'm' ;
+    private boolean aprobado = false ;
+    private String nombre = "Samuel Higuera" ;
+    private String saludo = "Hola, " + nombre ;
+    private double nota = 4.5 ;
+}
